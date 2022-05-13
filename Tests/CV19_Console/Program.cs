5fb18d797f034738b276456ed78027a6e4b43830
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-05-13 22:20:14 +0400

    Добавил тестовые проекты

---
+++ Tests/CV19_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CV19_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
