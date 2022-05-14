5580cb385e4fc0927f4c2dba260c33f6a9533110
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-05-14 20:28:35 +0400

    Добавил LambdaCommand

---
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,19 @@
+﻿using CV19.Infrastructure.Commands.Base;
+using System;
+
+namespace CV19.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
