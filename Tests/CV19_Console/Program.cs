8b91227e1016632223849a98ffece27da60e66d5
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-05-15 17:48:42 +0400

    Извлечение данных по датам записей

--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19_Console
 {
@@ -9,11 +13,34 @@ namespace CV19_Console
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            HttpClient client = new HttpClient();
-            var response = client.GetAsync(data_url).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine(csv_str);
+            var dates = GetDates();
+            Console.WriteLine(String.Join("\r\n", dates));
             Console.ReadLine();
         }
+        private static async Task<Stream> GetDataStream()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
     }
 }
