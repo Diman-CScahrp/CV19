2b0828bbe30cf044f4dd4fcb057a93060c67774e
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-05-15 18:26:12 +0400

    Тестирование бизнес-логики

--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -13,9 +13,10 @@ namespace CV19_Console
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            var dates = GetDates();
-            Console.WriteLine(String.Join("\r\n", dates));
-            Console.ReadLine();
+            var russia = GetData()
+                .First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(String.Join("\r\n", GetDates().Zip(russia.Counts, (date, count) => $"{date:dd.MM} - {count}")));
         }
         private static async Task<Stream> GetDataStream()
         {
@@ -32,7 +33,9 @@ namespace CV19_Console
             {
                 var line = data_reader.ReadLine();
                 if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line;
+                line = line.Replace("Bonaire,", "Bonaire -");
+                line = line.Replace("Saint Helena,", "Saint Helena -");
+                yield return line.Replace("Korea,", "Korea -");
             }
         }
         private static DateTime[] GetDates() => GetDataLines()
@@ -41,6 +44,21 @@ namespace CV19_Console
             .Skip(4)
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
+        
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
 
+            foreach(var row in lines)
+            {
+                var province = row[0].Trim();
+                var country = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(s => int.Parse(s)).ToArray();
+
+                yield return (country, province, counts);
+            }
+        }
     }
 }
