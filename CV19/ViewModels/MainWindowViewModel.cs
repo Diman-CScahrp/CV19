58caffc19ab5d030be52131e7ea7d56815288fa0
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-05-16 16:12:29 +0400

    Добавляем тестовые данные

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
@@ -8,6 +11,7 @@ namespace CV19.ViewModels
     internal class MainWindowViewModel : ViewModel
     {
         #region Properties
+
         #region Title
         private string _Title = "Анализ статистики CV19";
 
@@ -26,6 +30,18 @@ namespace CV19.ViewModels
             set => Set(ref _Status, value);
         }
         #endregion
+        #region TestDataPoints
+        /// <summary>
+        /// Тестовый набор данных
+        /// </summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+        #endregion
+
         #endregion
         #region Commands
         #region CloseApplicationCommand
@@ -43,6 +59,16 @@ namespace CV19.ViewModels
             #region Commands
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, OnCloseApplicationCommandCanExecute);
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x < 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(2 * Math.PI * x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+                TestDataPoints = data_points;
+            }
         }
     }
 }
