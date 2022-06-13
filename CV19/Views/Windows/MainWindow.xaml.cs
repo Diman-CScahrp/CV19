cdc1fc2fe620e1e2bab37358c78e90beae7eeca5
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-13 21:18:42 +0400

    Реализация фильтрации групп студентов

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CV19.Models.Decanat;
+using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls;
 
 namespace CV19
 {
@@ -23,6 +14,27 @@ namespace CV19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+            var filter_text = GroupNameFilterText.Text;
+            if (filter_text.Length == 0) return;
+
+            if(group.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if(group.Description != null && group.Description.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnGroupsFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
