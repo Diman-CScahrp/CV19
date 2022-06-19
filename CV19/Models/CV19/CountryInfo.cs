d7ca320fe1e3b29a6237d932768689e6c02cafb4
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-19 22:19:56 +0400

    Переопределение метода ToString в сервисе

--- CV19/Models/CV19/CountryInfo.cs
+++ CV19/Models/CV19/CountryInfo.cs
@@ -6,5 +6,9 @@ namespace CV19.Models.CV19
     {
         public string Name { get; set; }
         public IList<ProvinceInfo> Provinces { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} ({Provinces[0].Location.X},{Provinces[0].Location.X})";
+        }
     }
 }
