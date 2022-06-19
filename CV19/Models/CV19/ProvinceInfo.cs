831580ee864302c376dedcf802fac4e460d7f4ef
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-19 22:09:18 +0400

    CounrtyInfo and ProvinceInfo

---
+++ CV19/Models/CV19/ProvinceInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models.CV19
+{
+    internal class ProvinceInfo
+    {
+    }
+}
---
+++ Tests/CV19_Console/CountryInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models.CV19
+{
+    internal class CountryInfo
+    {
+        public string Name { get; set; }
+        public IList<ProvinceInfo> Provinces { get; set; }
+    }
+}
