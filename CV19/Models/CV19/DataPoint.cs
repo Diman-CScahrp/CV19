e7b735db32213d46fa08b58968d5aad052274986
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-21 23:28:50 +0400

    +Классы

---
+++ CV19/Models/CV19/DataPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models.CV19
+{
+    internal class DataPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
