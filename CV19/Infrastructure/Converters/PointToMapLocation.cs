cd4d82d91dcdc67dc049380b22049ec52465ca12
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-22 00:12:20 +0400

    новые классы

---
+++ CV19/Infrastructure/Converters/PointToMapLocation.cs
@@ -0,0 +1,26 @@
+﻿using MapControl;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CV19.Infrastructure.Converters
+{
+    [MarkupExtensionReturnType(typeof(PointToMapLocation))]
+    [ValueConversion(typeof(Point), typeof(Location))]
+    internal class PointToMapLocation : IValueConverter
+    {
+        public object Convert(object value, Type t, object p, CultureInfo c)
+        {
+            if(!(value is Point point)) return null;
+            return new Location(point.X, point.Y);
+        }
+
+        public object ConvertBack(object value, Type t, object p, CultureInfo c)
+        {
+            if (!(value is Location location)) return null;
+            return new Point(location.Latitude, location.Longitude);
+        }
+    }
+}
---
+++ CV19/Models/CV19/CofirmedCount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CV19.Models.CV19
+{
+    internal struct ConfirmedCount
+    {
+        public int Count { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
