04e5ff2832c1a242b5e0fee0d6c1e7243ff1b7e1
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-22 23:49:21 +0400

    Запуск и остановка хоста на входе и выходе соответсвенно

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -14,10 +14,25 @@ namespace CV19
     {
         public static bool IsDesignMode { get; private set; } = true;
 
-        protected override void OnStartup(StartupEventArgs e)
+        private static IHost __Host;
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+        protected override async void OnStartup(StartupEventArgs e)
         {
             IsDesignMode = false;
+            var host = Host;
             base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            __Host = null;
         }
 
         internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
