d5f47b9edae8a48d70a05631d6e6965893801798
DmitriySorokin <dima_sorokin54867@mail.ru>
2022-06-25 15:03:12 +0400

    класс

---
+++ CV19/Infrastructure/Converters/DebugConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class DebugConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            System.Diagnostics.Debugger.Break();
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            System.Diagnostics.Debugger.Break();
+            return value;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ CV19/Infrastructure/Converters/ParametricAddValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Data;
+using System.Windows;
+using System;
+using System.Globalization;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ParametricAddValueConverter : DependencyObject, IValueConverter
+    {
+        #region Value
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(double),
+                typeof(ParametricAddValueConverter),
+                new PropertyMetadata(
+                    default(double)
+                    ));
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        #endregion
+        public object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            var value = System.Convert.ToDouble(v, c);
+            return value + Value;
+        }
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            var value = System.Convert.ToDouble(v, c);
+
+            return value - Value;
+        }
+    }
+}
