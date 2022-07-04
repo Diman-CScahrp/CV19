362102e285a249d23c10efdf76910a01f20b8516
Dmitriy Sorokin <dima_sorokin54867@mail.ru>
2022-07-04 22:28:41 +0400

    new classes

---
+++ CV19/Services/Registrator.cs
@@ -0,0 +1,15 @@
+﻿using CV19.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CV19.Services
+{
+    internal static class Registrator
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+
+            services.AddSingleton<IDataService, DataCountriesService>();
+            return services;
+        }
+    }
+}
