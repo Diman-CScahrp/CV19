5b00d86d46cc76f7a90d088ba7aa1d1daed18f62
Dmitriy Sorokin <dima_sorokin54867@mail.ru>
2022-07-05 21:13:33 +0400

    WinForms

--- Tests/CV19WinFormsTest/Form1.cs
+++ Tests/CV19WinFormsTest/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,8 +20,24 @@ namespace CV19WinFormsTest
 
         private void StartButton_Click(object sender, EventArgs e)
         {
-
+            new Thread(ComputeValue).Start();
         }
-        private static 
+        private void ComputeValue()
+        {
+            var value = LongProcess(DateTime.Now);
+            SetResultValue(value);
+        }
+        private void SetResultValue(string value)
+        {
+            if (ResultLabel.InvokeRequired)
+                ResultLabel.Invoke(new Action<string>(SetResultValue), value);
+            else
+                ResultLabel.Text = value;
+        }
+        private static string LongProcess(DateTime Time)
+        {
+            Thread.Sleep(2000);
+            return $"Value: {Time}";
+        }
     }
 }
