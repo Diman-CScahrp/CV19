efea2959339b054d34b2fa8486503d984cccac33
Dmitriy Sorokin <dima_sorokin54867@mail.ru>
2022-07-17 15:52:10 +0400

    пром

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace OP
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Timer timer = new Timer();
+            timer.Tick = 2000;
+            timer.TimerCallBack += () =>
+            {
+                Console.Title = DateTime.Now.ToString();
+            };
+            timer.Start();
+
+            string msg = Console.ReadLine();
+            if (msg == "stop")
+                timer.Stop();
+        }
+    }
+    class Timer
+    {
+        public event Action TimerCallBack;
+        private bool isEnabled = true;
+        private Thread thread;
+        public int Tick { get; set; }
+
+        public void Start()
+        {
+            isEnabled = true;
+            thread = new Thread(() =>
+            {
+                while (isEnabled)
+                {
+                    Thread.Sleep(Tick);
+                    TimerCallBack.Invoke();
+                }
+            });
+            thread.Start();
+        }
+        public void Stop()
+        {
+            isEnabled = false;
+        }
+    }
+}
