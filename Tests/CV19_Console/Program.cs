ea755a6e10f66c6886948d5b44cf78cdaed97e12
Dmitriy Sorokin <dima_sorokin54867@mail.ru>
2022-07-17 15:33:54 +0400

    Блокировака и синхронизация потоков

--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Collections.Concurrent;
+using System.Linq;
 
 namespace CV19_Console
 {
@@ -10,13 +11,13 @@ namespace CV19_Console
         private static bool _ThreadUpdate = true;
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "Main thread";
+            //Thread.CurrentThread.Name = "Main thread";
 
-            var clock_thread = new Thread(ThreadMethod);
-            clock_thread.Name = "Second thread";
-            clock_thread.IsBackground = true;
-            clock_thread.Priority = ThreadPriority.AboveNormal;
-            clock_thread.Start(42);
+            //var clock_thread = new Thread(ThreadMethod);
+            //clock_thread.Name = "Second thread";
+            //clock_thread.IsBackground = true;
+            //clock_thread.Priority = ThreadPriority.AboveNormal;
+            //clock_thread.Start(42);
 
             //var count = 5;
             //var msg = "Hello World!";
@@ -32,26 +33,56 @@ namespace CV19_Console
             //    Console.WriteLine(i);
             //}
 
-            var values = new List<int>();
-            var threads = new Thread[10];
+            //var values = new List<int>();
+            //var threads = new Thread[10];
 
-            for (int i = 0; i < threads.Length; i++)
+            //for (int i = 0; i < threads.Length; i++)
+            //{
+            //    threads[i] = new Thread(() =>
+            //    {
+            //        for (int j = 0; j < 10; j++)
+            //        {
+            //            values.Add(Thread.CurrentThread.ManagedThreadId);
+            //        }
+            //    });
+            //}
+
+            //foreach (var thread in threads)
+            //{
+            //    thread.Start();
+            //}
+
+            //Mutex mutex = new Mutex();
+            //Semaphore semaphore = new Semaphore(0, 10);
+            //semaphore.WaitOne();
+            //semaphore.Release();
+
+            ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
+            EventWaitHandle eventWaitHandle = autoResetEvent;
+            
+            var test_threads = new Thread[10];
+            for (int i = 0; i < test_threads.Length; i++)
             {
-                threads[i] = new Thread(() =>
+                var local_i = i;
+                test_threads[i] = new Thread(() =>
                 {
-                    for (int j = 0; j < 10; j++)
-                    {
-                        values.Add(Thread.CurrentThread.ManagedThreadId);
-                    }
+                    Console.WriteLine("Поток: id:{0} - стартовал", Thread.CurrentThread.ManagedThreadId);
+                    
+                    eventWaitHandle.WaitOne();
+
+                    Console.WriteLine("value:{0}", local_i);
+                    Console.WriteLine("Поток: id:{0} - завершился", Thread.CurrentThread.ManagedThreadId);
+                    eventWaitHandle.Set();
                 });
+                test_threads[i].Start();
             }
 
-            foreach (var thread in threads)
-            {
-                thread.Start();
-            }
+            Console.ReadLine();
+            eventWaitHandle.Set();
 
-            Console.WriteLine(string.Join(",", values));
+            //Console.WriteLine(string.Join(",", values));
             Console.ReadLine();
         }
         private static void ThreadMethod(object parameter)
