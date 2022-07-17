ca72b2d1caf1ceed59f22e7f0a317df31d4d3e97
Dmitriy Sorokin <dima_sorokin54867@mail.ru>
2022-07-17 14:28:12 +0400

    Блокировака критической секции

--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Collections.Concurrent;
 
 namespace CV19_Console
 {
@@ -9,26 +11,59 @@ namespace CV19_Console
         {
             Thread.CurrentThread.Name = "Main thread";
 
-            var thread = new Thread(ThreadMethod);
-            thread.Name = "Second thread";
-            thread.IsBackground = true;
-            thread.Priority = ThreadPriority.AboveNormal;
-            thread.Start(42);
+            //var thread = new Thread(ThreadMethod);
+            //thread.Name = "Second thread";
+            //thread.IsBackground = true;
+            //thread.Priority = ThreadPriority.AboveNormal;
+            //thread.Start(42);
 
-            var count = 5;
-            var msg = "Hello World!";
-            var timeout = 150;
+            //var count = 5;
+            //var msg = "Hello World!";
+            //var timeout = 150;
 
-            new Thread(() => PrintMethod(msg, count, timeout)).Start();
+            //new Thread(() => PrintMethod(msg, count, timeout)) { IsBackground = true }.Start();
 
-            CheckThread();
+            //CheckThread();
 
-            for (int i = 0; i < 100; i++)
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Thread.Sleep(100);
+            //    Console.WriteLine(i);
+            //}
+
+            var values = new List<int>();
+            var threads = new Thread[10];
+
+            object lock_obj = new object();
+            for (int i = 0; i < threads.Length; i++)
             {
-                Thread.Sleep(100);
-                Console.WriteLine(i);
+                threads[i] = new Thread(() =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        lock (lock_obj)
+                            values.Add(Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(1);
+                    }
+                });
             }
 
+            Monitor.Enter(lock_obj);
+            try
+            {
+
+            }
+            finally
+            {
+                Monitor.Exit(lock_obj);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.WriteLine(string.Join(",", values));
             Console.ReadLine();
         }
         private static void PrintMethod(string message, int count, int timeout)
