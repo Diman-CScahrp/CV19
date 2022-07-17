76f297dbe1bf22edc7821c4f11ee417f3361c41d
Dmitriy Sorokin <dima_sorokin54867@mail.ru>
2022-07-17 16:54:24 +0400

    Заготовка асинхронного сервера

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,34 +4,28 @@
     {
         public static void Main()
         {
-            ManualResetEvent resetEvent = new ManualResetEvent(false);
-            List<string> tasks = new List<string>();
-
-            Thread thread_addTask = new Thread(() =>
+            List<int> ints = new List<int>();
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < threads.Length; i++)
             {
-                //
-                tasks.Add("new task");
-                resetEvent.Set();
-                Console.WriteLine("Sleeep 1");
-                Thread.Sleep(3000);
-                tasks.Add("one more task");
-                resetEvent.Set();
-                Console.WriteLine("Sleep 2");
-                Thread.Sleep(3000);
-                tasks.Add("old task");
-            });
-            thread_addTask.Start();
-
-            Thread thread_runTask = new Thread(() =>
-            {
-                while (true)
+                threads[i] = new Thread(() =>
                 {
-                    resetEvent.WaitOne();
-                    Console.WriteLine(tasks.Last());
-                    resetEvent.Reset();
-                }
-            });
-            thread_runTask.Start();
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Thread.Sleep(10);
+                        ints.Add(Thread.CurrentThread.ManagedThreadId);
+                    }
+                });
+            }
+
+            foreach (var item in threads)
+            {
+                item.Start();
+            }
+
+            Console.ReadLine();
+            Console.WriteLine(String.Join(",", ints));
+            Console.ReadLine();
         }
     }
 }
--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -33,54 +33,54 @@ namespace CV19_Console
             //    Console.WriteLine(i);
             //}
 
-            //var values = new List<int>();
-            //var threads = new Thread[10];
+            var values = new List<int>();
+            var threads = new Thread[10];
 
-            //for (int i = 0; i < threads.Length; i++)
-            //{
-            //    threads[i] = new Thread(() =>
-            //    {
-            //        for (int j = 0; j < 10; j++)
-            //        {
-            //            values.Add(Thread.CurrentThread.ManagedThreadId);
-            //        }
-            //    });
-            //}
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        values.Add(Thread.CurrentThread.ManagedThreadId);
+                    }
+                });
+            }
 
-            //foreach (var thread in threads)
-            //{
-            //    thread.Start();
-            //}
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
 
             //Mutex mutex = new Mutex();
             //Semaphore semaphore = new Semaphore(0, 10);
             //semaphore.WaitOne();
             //semaphore.Release();
 
-            ManualResetEvent manualResetEvent = new ManualResetEvent(false);
-            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+            //ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+            //AutoResetEvent autoResetEvent = new AutoResetEvent(false);
 
-            EventWaitHandle eventWaitHandle = autoResetEvent;
-            
-            var test_threads = new Thread[10];
-            for (int i = 0; i < test_threads.Length; i++)
-            {
-                var local_i = i;
-                test_threads[i] = new Thread(() =>
-                {
-                    Console.WriteLine("Поток: id:{0} - стартовал", Thread.CurrentThread.ManagedThreadId);
-                    
-                    eventWaitHandle.WaitOne();
+            //EventWaitHandle eventWaitHandle = autoResetEvent;
 
-                    Console.WriteLine("value:{0}", local_i);
-                    Console.WriteLine("Поток: id:{0} - завершился", Thread.CurrentThread.ManagedThreadId);
-                    eventWaitHandle.Set();
-                });
-                test_threads[i].Start();
-            }
+            //var test_threads = new Thread[10];
+            //for (int i = 0; i < test_threads.Length; i++)
+            //{
+            //    var local_i = i;
+            //    test_threads[i] = new Thread(() =>
+            //    {
+            //        Console.WriteLine("Поток: id:{0} - стартовал", Thread.CurrentThread.ManagedThreadId);
 
-            Console.ReadLine();
-            eventWaitHandle.Set();
+            //        eventWaitHandle.WaitOne();
+
+            //        Console.WriteLine("value:{0}", local_i);
+            //        Console.WriteLine("Поток: id:{0} - завершился", Thread.CurrentThread.ManagedThreadId);
+            //        eventWaitHandle.Set();
+            //    });
+            //    test_threads[i].Start();
+            //}
+
+            //Console.ReadLine();
+            //eventWaitHandle.Set();
 
             //Console.WriteLine(string.Join(",", values));
             Console.ReadLine();
